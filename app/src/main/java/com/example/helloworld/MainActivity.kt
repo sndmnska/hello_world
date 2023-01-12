55561e59d90bf9380cb2b20ddc13804c6c2139f2
dan <email@email.com>
2023-01-11 18:56:44 -0600

    Lab 1 Introduction complete

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -3,11 +3,18 @@ package com.example.helloworld
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.tapHereButton)
+        button.setOnClickListener{
+            Toast.makeText(this, "Hello Android!", Toast.LENGTH_LONG).show() // popup messages on android are toasts!
+        }
 
         Log.d("MainActivity", "***** Hello from main!")
     }
